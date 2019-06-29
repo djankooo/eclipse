689af93d66ca55d03de55fffa2523e0c7a8a9c85
djankooo <218202@student.pwr.edu.pl>
2019-06-29 16:18:01 +0200

    add - support different delimiters

--- src/main/java/task1/StringCalculator.java
+++ src/main/java/task1/StringCalculator.java
@@ -6,8 +6,15 @@ public class StringCalculator
     public static int add(String numbers){
 
         int sum = 0;
+        String pattern = "";
 
-        for(String s : numbers.split(",")) {
+        if(numbers.startsWith("//")) {
+            pattern = numbers.substring(numbers.indexOf("//") + 2);
+            pattern = pattern.substring(0, pattern.indexOf("\n"));
+        }
+
+
+        for(String s : numbers.split("[,\n"+pattern+"]")) {
                 if (isStringNumeric(s) && !s.isEmpty()) {
                     sum += Integer.valueOf(s);
                 }
--- src/test/java/task1/StringCalculatorTest.java
+++ src/test/java/task1/StringCalculatorTest.java
@@ -6,10 +6,40 @@ import static org.junit.Assert.*;
 public class StringCalculatorTest {
     @Test
     public void shouldAnswerWithTrue() {
+
+        // 1
+        assertEquals(0, StringCalculator.add(""));
+        assertEquals(1, StringCalculator.add("1"));
+        assertEquals(3, StringCalculator.add("1,2"));
+
         assertEquals(10, StringCalculator.add("asd,10,0"));
         assertEquals(10, StringCalculator.add("asd,1,,,9,,,,0"));
         assertEquals(10, StringCalculator.add(",,///,..,01,,asd,,08,01"));
         assertEquals(10, StringCalculator.add("asd,1,2,2,,,5,0"));
         assertEquals(0, StringCalculator.add("0,10asd,,,0,000,0,0"));
+
+
+        // 2
+        assertEquals(6, StringCalculator.add("1\n2,3"));
+
+        assertEquals(10, StringCalculator.add("asd,10,\n0"));
+        assertEquals(10, StringCalculator.add("\nasd,1,,\n\n,\n9,,,,0\n"));
+        assertEquals(10, StringCalculator.add(",,///,..,\n01\n,,asd,,08,01"));
+        assertEquals(10, StringCalculator.add("asd,1,2,\n\n\n2,,,5,0"));
+        assertEquals(0, StringCalculator.add("0,10asd,\n\n,,\n0,0\n00,0,0"));
+
+
+        //3
+        assertEquals(3, StringCalculator.add("//;\n1;2" ));
+
+        assertEquals(3, StringCalculator.add("//!\n1!2" ));
+        assertEquals(3, StringCalculator.add("//$$\n1$$2" ));
+        assertEquals(6, StringCalculator.add("//$$\n1$$2\n\n3" ));
+        assertEquals(6, StringCalculator.add("//$$\n1$$2,,,$$\n3" ));
+
+
+        //4
+        
+
     }
 }
